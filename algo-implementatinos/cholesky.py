e830f3915e9845d8ae52439cd2bb25882e386580
artem <m.osekin77@gmail.com>
2022-11-05 14:24:36 +0300

    svd omg////

--- algo-implementatinos/cholesky.py
+++ algo-implementatinos/cholesky.py
@@ -18,13 +18,58 @@ def cholesky(a):
     return L
 
 
-H = [[5.2,3,0.5,1,2],
+H = [[3.2,3,0.5,1,2],
      [3,6.3,-2,4,0],
      [0.5,-2,8,-3.1,3],
      [1,4,-3.1,7.6,2.6],
      [2,0,3,2.6,15]]
 
-
 L = cholesky(H)
-print("Cholesky decomposition of matrix H is: ")
-print(L)
+# print(np.dot(L, np.transpose(L)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
